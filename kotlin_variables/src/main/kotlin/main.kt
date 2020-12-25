d1c271a3648dd11166166fea97f38a62f2b3c7e1
athamour1 <thanosmour.tk@gmail.com>
2020-12-25 14:14:47 +0200

    Playing with variables

---
+++ kotlin_variables/src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("\n")
+    val name: String = "Thanos"
+    println(name)
+    println("\n")
+    /*val is non-alterable variable*/
+
+    var x: String = "kati"
+    println(x)
+    println("\n")
+    x = "thats_jst_a_test"
+    println(x)
+    println("\n")
+
+
+}

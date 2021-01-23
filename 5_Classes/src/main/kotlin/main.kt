c2246e0850fd3fba20b4ec3828cef4c94568ee3c
repl.it user <replituser@example.com>
2021-01-23 09:31:48 +0000

    Update README.md

--- 5_Classes/src/main/kotlin/main.kt
+++ 5_Classes/src/main/kotlin/main.kt
@@ -2,7 +2,10 @@ fun main(args: Array<String>) {
   val person=person("Thanos", "Mourtzoukos")
   println(person.firstName)
   println(person.lastName)
+
   val person2=person()
   println(person2.firstName)
   println(person2.lastName)
+  person.nickName = "Malakas"
+  person.nickName = "GEIASOOO"
 }
--- 5_Classes/src/main/kotlin/person.kt
+++ 5_Classes/src/main/kotlin/person.kt
@@ -1,3 +1,7 @@
-class person(val firstName:String, val lastName:String){
-    
+class person(val firstName:String = "Kati", val lastName:String = "Thanos"){
+    var nickName: String? = null
+        set(value) {
+            field = value
+            println("LOG : The new nickname is $value")
+        }
 }

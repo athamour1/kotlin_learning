82935d379715613a77431b9efee21417dddfc7a8
Thanos Mourtzoukos <thanosmour.tk@gmail.com>
2021-01-23 09:28:40 +0000

    playing with classes

--- 5_Classes/src/main/kotlin/main.kt
+++ 5_Classes/src/main/kotlin/main.kt
@@ -2,4 +2,7 @@ fun main(args: Array<String>) {
   val person=person("Thanos", "Mourtzoukos")
   println(person.firstName)
   println(person.lastName)
+  val person2=person()
+  println(person2.firstName)
+  println(person2.lastName)
 }
